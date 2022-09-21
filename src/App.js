84c74c4eec91bcac64463e5c4773f8010feccc36
shollid1n <100890984+shollid1n@users.noreply.github.com>
2022-09-21 08:12:18 -0500

    updated links

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://shalinda-portfolio.netlify.app/" target="_blank">
+        <a
+          href="https://shalinda-portfolio.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Shalinda Holliday
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/shollid1n/react-weather-app.git"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>
