9dde1b0add458792d516c58d768487b47dd21d92
shollid1n <100890984+shollid1n@users.noreply.github.com>
2022-09-22 01:28:19 -0500

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://shalinda-portfolio.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Shalinda Holliday
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/shollid1n/react-weather-app.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://shalinda-portfolio.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Shalinda Holliday
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/shollid1n/react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
